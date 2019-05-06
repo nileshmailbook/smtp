14f358b8991e6898f76f1577542724fe308a4c92
nilesh <nilesh@xebecindia.com>
2019-05-06 14:44:29 +0530

    namespace error removed

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-#Laravel SMTP Configuration
-## this package allow user to create own smtp in existing project and allow sending mail through it.
-## simply call /smtp route and fill the details of your smtp and and tested on their.
-## this package extend the laravel mail on the fly, no need to configure .env or config/mail.php
-## hassle free package available form laravel 5.8.* 
+##Laravel SMTP Configuration
+#install this package "composer require neil/mail-smtp"
+#this package allow user to create own smtp in existing project and allow sending mail through it.
+# simply call /smtp route and fill the details of your smtp and and tested on their.
+# this package extend the laravel mail on the fly, no need to configure .env or config/mail.php
+# hassle free package available form laravel 5.8.* 
--- src/routes/web.php
+++ src/routes/web.php
@@ -15,10 +15,10 @@ Route::group(['prefix'=>'smtp', 'middleware'=>['web','auth']], function(){
 
 //smtp configuration
 
-Route::get('/', 'Neil\mailSetting\Controllers\SettingController@index')->name('smtp.index');
-Route::post('/store', 'Neil\mailSetting\Controllers\SettingController@store')->name('smtp.store');
-Route::post('/update/{id}', 'Neil\mailSetting\Controllers\SettingController@update')->name('smtp.update');
+Route::get('/', 'Neil\MailSetting\Controllers\SettingController@index')->name('smtp.index');
+Route::post('/store', 'Neil\MailSetting\Controllers\SettingController@store')->name('smtp.store');
+Route::post('/update/{id}', 'Neil\MailSetting\Controllers\SettingController@update')->name('smtp.update');
 
 //smtp test check
-Route::post('/check', 'Neil\mailSetting\Controllers\SettingController@smtpCheck')->name('smtp.check'); 
+Route::post('/check', 'Neil\MailSetting\Controllers\SettingController@smtpCheck')->name('smtp.check'); 
 });
