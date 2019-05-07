d3a17e1802b54c269898521f1297d42dd34fbc4e
nilesh <nilesh@xebecindia.com>
2019-05-07 12:08:43 +0530

    testing performed

--- src/MailSettingServiceProvider.php
+++ src/MailSettingServiceProvider.php
@@ -14,10 +14,12 @@ class MailSetServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {   //loading here
+    {   
+        //loading here
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views','mailsetting');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+
         //publish view
         $this->publishes([
         __DIR__.'/resources/views' => resource_path('views/vendor/mailsetting'),
